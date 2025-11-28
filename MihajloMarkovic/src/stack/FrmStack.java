9f53764492fb010f94b6f87bcf6c650af909c73b
mihai <mihai@IvanaMikanovic>
2025-11-28 14:09:55 +0100

    Implementirana kompletna logika Stack aplikacije: povezivanje FrmStack i DlgStack, kreiranje i izuzimanje Doughnut objekata sa validacijom i LIFO upravljanjem.

--- MihajloMarkovic/src/stack/FrmStack.java
+++ MihajloMarkovic/src/stack/FrmStack.java
@@ -3,15 +3,19 @@ package stack;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import geometry.Doughnut;
+import geometry.Point;
 
 public class FrmStack extends JFrame
 {
@@ -64,10 +68,68 @@ public class FrmStack extends JFrame
 		panelSouth.setBackground(new Color(240,250,200));
 		
 		JButton addButton = new JButton("ADD");
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				DlgStack dialog = new DlgStack();
+				dialog.setVisible(true);
+				
+				if(dialog.isConfirmation()) 
+				{
+					Doughnut doughnut = new Doughnut(
+					        Integer.parseInt(dialog.getTxtRadius().getText()),
+					        Integer.parseInt(dialog.getTxtInnerRadius().getText()),
+					        new Point(
+					            Integer.parseInt(dialog.getTxtXCoordinate().getText()),
+					            Integer.parseInt(dialog.getTxtYCoordinate().getText())
+					        )
+					);
+					
+					listStack.add(0,  doughnut);
+
+				}
+			}
+			
+		});
 		panelSouth.add(addButton);
 		addButton.setBackground(Color.GREEN);
 		
 		JButton deleteButton = new JButton("DELETE");
+		deleteButton.addActionListener(new ActionListener() 
+		{
+		    public void actionPerformed(ActionEvent e) 
+		    {
+		        if (listStack.isEmpty()) 
+		        {
+		            JOptionPane.showMessageDialog(null, "THE STACK IS EMPTY!", "ERROR", JOptionPane.ERROR_MESSAGE);
+		        } 
+		        else 
+		        {
+		            DlgStack dialog = new DlgStack();
+
+		           
+		            dialog.getTxtXCoordinate().setText(String.valueOf(listStack.getElementAt(0).getCenter().getX()));
+		            dialog.getTxtYCoordinate().setText(String.valueOf(listStack.getElementAt(0).getCenter().getY()));
+		            dialog.getTxtRadius().setText(String.valueOf(listStack.getElementAt(0).getRadius()));
+		            dialog.getTxtInnerRadius().setText(String.valueOf(listStack.getElementAt(0).getInnerRadius()));
+
+		           
+		            dialog.getTxtXCoordinate().setEditable(false);
+		            dialog.getTxtYCoordinate().setEditable(false);
+		            dialog.getTxtRadius().setEditable(false);
+		            dialog.getTxtInnerRadius().setEditable(false);
+
+		            dialog.setVisible(true);
+
+		           
+		            if (dialog.isConfirmation()) 
+		            {
+		                listStack.removeElementAt(0);
+		            }
+		        }
+		    }
+		});
+
 		panelSouth.add(deleteButton);
 		deleteButton.setBackground(Color.RED);
 		
